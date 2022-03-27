d6bc70bc43cd2b8cdd4bcf6253a8dec95848d9a6
Lee Kang Wei <65090312+lkwlkww@users.noreply.github.com>
2022-03-27 17:22:14 +0800

    Added level attribute for pokemon

--- pokemon/models.py
+++ pokemon/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Pokemon(models.Model):
@@ -9,8 +10,11 @@ class Pokemon(models.Model):
     defense = models.IntegerField()
     type = models.CharField(max_length=100)
 
-    ## Randomly generated for a Pokemon upon its capture
-    #level = models.IntegerField()
+    # Default level is 0. Changed to a randomly generated level from 0-100 upon a Pokemon's capture.
+    level = models.IntegerField(default=0)
+
+    ## The user that a Pokemon would belong to after it has been captured
+    # user = models.ForeignKey(User)
 
     def __str__(self):
         return self.name
