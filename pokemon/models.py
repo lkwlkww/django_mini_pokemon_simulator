4de8629743e77129f8125965dbe41b7c9eb93797
Lee Kang Wei <65090312+lkwlkww@users.noreply.github.com>
2022-03-28 23:11:37 +0800

    added CapturedPokemon and WildPokemon models

--- pokemon/models.py
+++ pokemon/models.py
@@ -4,25 +4,23 @@ from django.conf import settings
 
 class Pokemon(models.Model):
     # ATTRIBUTES # 
-    # Attributes found within the csv.
     name = models.CharField(max_length=100)
     hp = models.IntegerField()
     attack = models.IntegerField()
     defense = models.IntegerField()
     type = models.CharField(max_length=100)
 
-    # Default level is 0. Changed to a randomly generated level from 0-100 upon a Pokemon's capture.
-    level = models.IntegerField(default=0)
-
-    # The user that a Pokemon would belong to after it has been captured
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.SET_NULL)
-
-    # METHODS
-
     def __str__(self):
         return self.name
 
     def __unicode__(self):
+        return self.name
+
+class CapturedPokemon(Pokemon):
+    level = models.IntegerField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
         return self.name
 
 class User(AbstractUser):
