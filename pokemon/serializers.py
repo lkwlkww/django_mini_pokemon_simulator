3be27d3713c03bf050e6f54e69aae06fa87e150a
Lee Kang Wei <65090312+lkwlkww@users.noreply.github.com>
2022-03-28 23:11:08 +0800

    created CapturedPokemonSerializer

--- pokemon/serializers.py
+++ pokemon/serializers.py
@@ -12,8 +12,17 @@ class PokemonSerializer(serializers.Serializer):
         instance.name = validated_data.get('name', instance.name)
         instance.hp = validated_data.get('hp', instance.hp)
         instance.attack = validated_data.get('attack', instance.attack)
-        instance.defense = validated_data.get('defesnse', instance.attack)
+        instance.defense = validated_data.get('defense', instance.defense)
         instance.type = validated_data.get('type', instance.type)
+        instance.level = validated_data.get('level', instance.level)
 
         instance.save()
         return instance
+
+"""
+Serializes the level and user of the captured pokemon, in addition to the serialized
+fields in PokemonSerializer.
+"""
+class CapturedPokemonSerializer(PokemonSerializer):
+    level = serializers.IntegerField()
+    user = serializers.CharField(max_length=100)
