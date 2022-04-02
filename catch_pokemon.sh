71e17d50d5d1c057b8a6bcedca724d6e5493b95e
lkwlkww <65090312+lkwlkww@users.noreply.github.com>
2022-04-02 14:04:44 +0800

    Delete catch_pokemon.sh

--- catch_pokemon.sh
+++
@@ -1,1 +0,0 @@
-curl -X GET http://127.0.0.1:8000/pokemon/catchpokemon/ -H 'Authorization: JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjQ4NzU5MDkwLCJqdGkiOiJlYTBmOWE0ZjYyYzg0MmJiYTkwYzAwMDA4M2QzMTI5ZSIsInVzZXJfaWQiOjF9.tTMdjBtvr1IOWGaxKjDMcVtKSdjOqvLboB_jNSTD9d8' > apiresponse.html && open apiresponse.html
