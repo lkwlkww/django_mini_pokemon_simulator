a787356f351088ccd2be41a311b341bb96ae93c6
lkwlkww <65090312+lkwlkww@users.noreply.github.com>
2022-04-02 14:05:01 +0800

    Delete get_my_pokemon.sh

--- get_my_pokemon.sh
+++
@@ -1,1 +0,0 @@
-curl -X GET http://127.0.0.1:8000/pokemon/mypokemon/ -H 'Authorization: JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjQ4NjUzNDg0LCJqdGkiOiJkMDNiNjc1Y2I1NWY0ZGI0OWY2ZDEyN2QzMTEyNDhhMyIsInVzZXJfaWQiOjF9.jDEqJhBxMW19rPRtexmSjRkzRO2fPMBKAI9jcz28KDo' > apiresponse.html && open apiresponse.html
